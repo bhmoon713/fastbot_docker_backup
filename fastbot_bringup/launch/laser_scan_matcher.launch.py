dbb2cf5e43c3c5cdc6dcad1af1272fbbeb5b0351
Rodrigo <roalgoal@gmail.com>
2024-10-23 10:49:12 +0200

    remapped scan topic.

--- fastbot_bringup/launch/laser_scan_matcher.launch.py
+++ fastbot_bringup/launch/laser_scan_matcher.launch.py
@@ -14,35 +14,35 @@ from launch.conditions import IfCondition
 
 def launch_setup(context, *args, **kwargs):
     ####### DATA INPUT ##########
-    use_sim_time=LaunchConfiguration('use_sim_time')
+    use_sim_time = LaunchConfiguration("use_sim_time")
     # This is to access the argument variables. Otherwise we cant access the values
-    robot_name = LaunchConfiguration('robot_name').perform(context)
-
+    robot_name = LaunchConfiguration("robot_name").perform(context)
 
     odometry_node = Node(
-        package='ros2_laser_scan_matcher',
-        condition=IfCondition(PythonExpression(['not ', use_sim_time])),
-        parameters=[{
-                'base_frame': robot_name+'_base_link',
-                'odom_frame': robot_name+'_odom',
-                'laser_frame': robot_name+'_lidar',
-                'robot_name': robot_name,
-                'publish_odom': robot_name+'/odom'
-            }],
-        executable='laser_scan_matcher',
-        name='odometry_publisher',
+        package="ros2_laser_scan_matcher",
+        condition=IfCondition(PythonExpression(["not ", use_sim_time])),
+        parameters=[
+            {
+                "base_frame": robot_name + "_base_link",
+                "odom_frame": robot_name + "_odom",
+                "laser_frame": robot_name + "_lidar",
+                "robot_name": robot_name,
+                "publish_odom": robot_name + "/odom",
+            }
+        ],
+        remappings=[("/scan", robot_name + "/scan")],
+        executable="laser_scan_matcher",
+        name="odometry_publisher",
     )
-
 
     return [odometry_node]
 
-def generate_launch_description(): 
 
-    robot_name_arg = DeclareLaunchArgument('robot_name', default_value='boxbot_X')
-    use_sim_time_arg = DeclareLaunchArgument(name='use_sim_time', default_value='False')
+def generate_launch_description():
 
-    return LaunchDescription([
-        robot_name_arg,
-        use_sim_time_arg,
-        OpaqueFunction(function = launch_setup)
-        ])
+    robot_name_arg = DeclareLaunchArgument("robot_name", default_value="fastbot_X")
+    use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="False")
+
+    return LaunchDescription(
+        [robot_name_arg, use_sim_time_arg, OpaqueFunction(function=launch_setup)]
+    )
