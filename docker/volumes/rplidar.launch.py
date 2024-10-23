e0d616d726f395561b2b2949a7b5dcff713ec2f1
Rodrigo <roalgoal@gmail.com>
2024-10-23 11:57:32 +0200

    rplidar launch fix 4.

--- docker/volumes/rplidar.launch.py
+++ docker/volumes/rplidar.launch.py
@@ -1,37 +1,34 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, LifecycleNode
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+
+
+def launch_setup(context, *args, **kwargs):
+    """Accept arguments from a parent launch (include)"""
+    robot_name = LaunchConfiguration("robot_name").perform(context)
+
+    rplidar_node = LifecycleNode(
+        package="rplidar_composition",
+        executable="rplidar_composition",
+        name="rplidar_node",
+        output="screen",
+        parameters=[
+            {
+                "serial_port": "/dev/ttyUSB0",
+                "serial_baudrate": 115200,  # A1 / A2
+                "frame_id": robot_name + "_lidar",
+                "inverted": False,
+                "angle_compensate": True,
+            }
+        ],
+    )
+
+    return [rplidar_node]
 
 
 def generate_launch_description():
+
     robot_name_arg = DeclareLaunchArgument("robot_name", default_value="fastbot_1")
 
-    return LaunchDescription(
-        [
-            robot_name_arg,
-            Node(
-                name="rplidar_composition",
-                package="rplidar_ros",
-                executable="rplidar_composition",
-                output="screen",
-                parameters=[
-                    {
-                        "serial_port": "/dev/ttyUSB0",
-                        "serial_baudrate": 115200,  # A1 / A2
-                        "frame_id": LaunchConfiguration(
-                            "robot_name"
-                        ),  # Just use the LaunchConfiguration here
-                        "inverted": False,
-                        "angle_compensate": True,
-                    }
-                ],
-                remappings=[
-                    (
-                        "{}_lidar".format(LaunchConfiguration("robot_name")),
-                        "frame_id",
-                    )  # Remapping frame_id correctly
-                ],
-            ),
-        ]
-    )
+    return LaunchDescription([robot_name_arg, OpaqueFunction(function=launch_setup)])
