a40d8e8572a714461931996189453095f2a1b6a2
Rodrigo <roalgoal@gmail.com>
2024-10-23 11:50:45 +0200

    rplidar launch fix 3.

--- docker/volumes/rplidar.launch.py
+++ docker/volumes/rplidar.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 
 
@@ -19,15 +19,18 @@ def generate_launch_description():
                     {
                         "serial_port": "/dev/ttyUSB0",
                         "serial_baudrate": 115200,  # A1 / A2
-                        "frame_id": TextSubstitution(
-                            text="{}{}".format(
-                                LaunchConfiguration("robot_name").perform(None),
-                                "_lidar",
-                            )
-                        ),
+                        "frame_id": LaunchConfiguration(
+                            "robot_name"
+                        ),  # Just use the LaunchConfiguration here
                         "inverted": False,
                         "angle_compensate": True,
                     }
+                ],
+                remappings=[
+                    (
+                        "{}_lidar".format(LaunchConfiguration("robot_name")),
+                        "frame_id",
+                    )  # Remapping frame_id correctly
                 ],
             ),
         ]
