5cb80eda0c2c26429e866a3daa3513ac78453b33
Rodrigo <roalgoal@gmail.com>
2024-10-23 11:45:17 +0200

    rplidar launch fix.

--- docker/volumes/rplidar.launch.py
+++ docker/volumes/rplidar.launch.py
@@ -1,15 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
     robot_name_arg = DeclareLaunchArgument("robot_name", default_value="fastbot_1")
 
+    frame_id_arg = TextSubstitution(
+        text="{}_lidar".format(LaunchConfiguration("robot_name"))
+    )
+
     return LaunchDescription(
         [
-            robot_name_arg,  # Add the launch argument declaration to the LaunchDescription
+            robot_name_arg,
             Node(
                 name="rplidar_composition",
                 package="rplidar_ros",
@@ -19,8 +23,7 @@ def generate_launch_description():
                     {
                         "serial_port": "/dev/ttyUSB0",
                         "serial_baudrate": 115200,  # A1 / A2
-                        "frame_id": LaunchConfiguration("robot_name")
-                        + "_lidar",  # Retrieve the value correctly
+                        "frame_id": frame_id_arg,  # Use the combined frame_id
                         "inverted": False,
                         "angle_compensate": True,
                     }
