bcdecdde6618e56306bff406a4579d8a1167280a
Rodrigo <roalgoal@gmail.com>
2024-10-29 14:51:56 +0100

    flipped lidar TF 180º and mounted xacro as volume.

--- docker/volumes/rplidar.launch.py
+++ docker/volumes/rplidar.launch.py
@@ -18,7 +18,7 @@ def launch_setup(context, *args, **kwargs):
                 "serial_port": "/dev/ttyUSB0",
                 "serial_baudrate": 115200,  # A1 / A2
                 "frame_id": robot_name + "_lidar_frame",
-                "inverted": True,
+                "inverted": False,
                 "angle_compensate": True,
             }
         ],
