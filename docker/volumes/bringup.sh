d131400100a25ddfa30d1a24de98ec3f316df7e0
Rodrigo <roalgoal@gmail.com>
2024-10-30 14:55:00 +0100

    added variable robot name in compose and bringup.sh

--- docker/volumes/bringup.sh
+++ docker/volumes/bringup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-ros2 launch fastbot_bringup bringup.launch.xml robot_name:=fastbot_1 &
+# Use the ROBOT_NAME environment variable, or default to fastbot_1 if unset
+robot_name=${ROBOT_NAME:-fastbot_1}
+
+ros2 launch fastbot_bringup bringup.launch.xml robot_name:=$robot_name &
 sleep 5
-ros2 launch fastbot_bringup rplidar.launch.xml robot_name:=fastbot_1
+ros2 launch fastbot_bringup rplidar.launch.xml robot_name:=$robot_name
