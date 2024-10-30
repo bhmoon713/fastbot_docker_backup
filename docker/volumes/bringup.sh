45e212515c6116f813ad2f36cf5953dd323e2cf2
Rodrigo <roalgoal@gmail.com>
2024-10-30 12:59:17 +0100

    added &

--- docker/volumes/bringup.sh
+++ docker/volumes/bringup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ros2 launch fastbot_bringup bringup.launch.xml robot_name:=fastbot_1
+ros2 launch fastbot_bringup bringup.launch.xml robot_name:=fastbot_1 &
 sleep 5
 ros2 launch fastbot_bringup rplidar.launch.xml robot_name:=fastbot_1
