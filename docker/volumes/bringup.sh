75c13cea6ac9855f4234c7376ed7990ac06c292d
Rodrigo <roalgoal@gmail.com>
2025-01-30 17:58:26 +0100

    Update Dockerfile and docker-compose files: LSLidar

--- docker/volumes/bringup.sh
+++ docker/volumes/bringup.sh
@@ -3,6 +3,6 @@
 # Use the ROBOT_NAME environment variable, or default to fastbot_1 if unset
 robot_name=${ROBOT_NAME:-fastbot_1}
 
-ros2 launch fastbot_bringup rplidar.launch.xml robot_name:=$robot_name &
-sleep 7
+# ros2 launch fastbot_bringup rplidar.launch.xml robot_name:=$robot_name &
+# sleep 7
 ros2 launch fastbot_bringup bringup.launch.xml robot_name:=$robot_name
